df31acb585cb654a540737de4b7adc26dca34e85
Matheus Bloize <mbloizepd@gmail.com>
2025-02-21 11:47:47 -0300

    feat: create migrations

---
+++ src/main/resources/db/changelog/migrations/db.changelog-202502211144.sql
@@ -0,0 +1,14 @@
+--liquibase formatted sql
+--changeset matheus:202502211144
+--comment: cards table create
+
+CREATE TABLE CARDS (
+    id BIGSERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    "order" INT NOT NULL,
+    board_column_id BIGINT NOT NULL,
+    CONSTRAINT boards_columns__cards_fk FOREIGN KEY (board_column_id) REFERENCES BOARDS_COLUMNS(id) ON DELETE CASCADE
+);
+
+--rollback DROP TABLE CARDS
